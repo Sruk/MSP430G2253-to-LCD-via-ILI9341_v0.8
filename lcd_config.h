52263104fe17a6afad2f1fb4cce0e10a89382f3a
Mario <flash.mario@gmail.com>
2014-04-06 16:25:49 +0200

    Create lcd_config.h

---
+++ lcd_config.h
@@ -0,0 +1,16 @@
+/*
+ * lcd_confg.h
+ *
+ *  This software is distributed under the GPL.
+ *  Software has been tested under real circumstances. Use this software on your on risk.
+ *  Any source code resemblance to the any property of the third party's is purely coincidental.
+ *
+ *  Created on: 10. 3. 2014.
+ *      Author: Sruk
+ *
+ *      using "ILI9340C" LCD encoder IC
+ */
+
+void init_ILI9340C(void);
+void set_gamma(void);
+void color_invert(void);
