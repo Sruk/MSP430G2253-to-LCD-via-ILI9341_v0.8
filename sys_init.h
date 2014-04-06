bdda4d6150d026543d1ae934efd3583cd7d30337
Mario <flash.mario@gmail.com>
2014-04-06 16:35:41 +0200

    Create sys_init.h

---
+++ sys_init.h
@@ -0,0 +1,17 @@
+/*
+ * sys_init.h
+ *
+ *  This software is distributed under the GPL.
+ *  Software has been tested under real circumstances. Use this software on your on risk.
+ *  Any source code resemblance to the any property of the third party's is purely coincidental.
+ *
+ *  Created on: 10. 3. 2014.
+ *      Author: Sruk
+ */
+
+#ifndef SYS_INIT_H_
+#define SYS_INIT_H_
+
+void sys_init(void);
+
+#endif /* SYS_INIT_H_ */
