62f7d252da4bdb6614ceb0327a82555db8ecab75
Mario <flash.mario@gmail.com>
2014-04-06 16:37:04 +0200

    Create timer_init.h

---
+++ timer_init.h
@@ -0,0 +1,18 @@
+/*
+ * timer_init.h
+ *
+ *  This software is distributed under the GPL.
+ *  Software has been tested under real circumstances. Use this software on your on risk.
+ *  Any source code resemblance to the any property of the third party's is purely coincidental.
+ *
+ *  Created on: 10. 3. 2014.
+ *      Author: Sruk
+ */
+
+#ifndef TIMER_INIT_H_
+#define TIMER_INIT_H_
+
+void timer_init(void);
+void timer_wait(unsigned int);
+
+#endif /* TIMER_INIT_H_ */
