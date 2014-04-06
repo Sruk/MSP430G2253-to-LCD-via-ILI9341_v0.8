971ae80b55fa5adc0959c9985538456705b22ba3
Mario <flash.mario@gmail.com>
2014-04-06 16:28:55 +0200

    Create LCD_incl.h

---
+++ LCD_incl.h
@@ -0,0 +1,21 @@
+/*
+ * LCD_incl.h
+ *
+ *  This software is distributed under the GPL.
+ *  Software has been tested under real circumstances. Use this software on your on risk.
+ *  Any source code resemblance to the any property of the third party's is purely coincidental.
+ *
+ *  Created on: 10. 3. 2014.
+ *      Author: Sruk
+ */
+
+#ifndef LCD_INCL_H_
+#define LCD_INCL_H_
+
+#include "sys_init.h"
+#include "timer_init.h"
+#include "lcd_config.h"
+#include "lcd_driver.h"
+#include "lcd_config.h"
+
+#endif /* LCD_INCL_H_ */
